eeb8ae3801ac9be45d679329c69f4ef9a68aaefc
Tahmid Imran <tahmidimran1@gmail.com>
2022-06-20 23:08:52 -0700

    First implementation of scraping and transferring to excel

--- scrape.py
+++ scrape.py
@@ -1,5 +1,44 @@
 import requests
-url = 'https://coinmarketcap.com/historical/20180201/'
-data = requests.get(url)
-text = data.text
-print(text[len(text) - 1])
+from bs4 import BeautifulSoup
+from pprint import pprint
+from openpyxl import Workbook
+
+
+def getRows():
+    rows = html.select('.cmc-table-row')
+    data = []
+    for row in rows[0:20]:
+        name = row.select(
+            ".cmc-table__cell--sort-by__name")[0].get_text()
+        ticker = row.select(
+            ".cmc-table__cell--sort-by__symbol")[0].get_text()
+        price = row.select(
+            ".cmc-table__cell--sort-by__price")[0].get_text()
+        data.append({'name': name, 'ticker': ticker, 'price': price})
+        print(f'({ticker}) price: {price}')
+    return data
+
+
+def dataToExcel(data, date):
+
+    sheet['A1'] = 'name'
+    sheet['B1'] = 'ticker'
+    sheet['C1'] = 'price'
+    count = 2
+    for d in data:
+        sheet[f'A{count}'] = d['name']
+        sheet[f'B{count}'] = d['ticker']
+        sheet[f'C{count}'] = d['price']
+        count += 1
+        print(d)
+    workbook.save(filename=f'{date}.xlsx')
+
+
+if __name__ == "__main__":
+    date = input("Please enter a date in the format yyyymmdd: ")
+    url = f'https://coinmarketcap.com/historical/{date}/'
+    data = requests.get(url)
+    html = BeautifulSoup(data.text, 'html.parser')
+    workbook = Workbook()
+    sheet = workbook.active
+    dataToExcel(getRows(), date)
